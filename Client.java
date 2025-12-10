3361a2b4d31e6d173a1204dc1f9842c828bd4c1a
helen <helenkokob317@gmail.com>
2025-12-10 15:23:47 +0300

    add file streams for read and write in socket

--- Client.java
+++ Client.java
@@ -1,7 +1,13 @@
+import java.io.*;
 import java.net.*;
 public class Client {
     public static void main(String[]args)throws Exception{
        Socket socket=new Socket("localhost",5000);
        System.out.println("Connected!"); 
+      
+       BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+       BufferedReader keyboard=new BufferedReader(new InputStreamReader(System.in));
+PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
+
     }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.net.*;
 public class Server{
     public static void main(String[]args)throws Exception{
@@ -6,5 +7,8 @@ System.out.println("Waiting...");
 Socket socket=server.accept();
 System.out.println("Client connected.");
 
+ BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+       BufferedReader keyboard=new BufferedReader(new InputStreamReader(System.in));
+PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
     }
 }
