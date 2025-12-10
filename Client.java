71d73468922aff2850496ded9e5f4e7c64c28968
helen <helenkokob317@gmail.com>
2025-12-10 15:05:26 +0300

    Add socket

--- Client.java
+++ Client.java
@@ -1,7 +1,7 @@
 import java.net.*;
-import java.io.*;
 public class Client {
     public static void main(String[]args)throws Exception{
-        
+       Socket socket=new Socket("localhost",5000);
+       System.out.println("Connected!"); 
     }
 }
--- Server.java
+++ Server.java
@@ -1,7 +1,10 @@
-import java.io.*;
 import java.net.*;
 public class Server{
     public static void main(String[]args)throws Exception{
+ServerSocket server=new ServerSocket(5000);
+System.out.println("Waiting...");
+Socket socket=server.accept();
+System.out.println("Client connected.");
 
     }
 }
