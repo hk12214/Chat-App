2e0989aa23a4e58f74260b573252b5c2c11c6d96
helen <helenkokob317@gmail.com>
2025-12-14 21:51:39 +0300

    import packages for GUI

---
+++ ChatClient.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ChatClient {
+    public static void main(String[]args)throws Exception{
+       Socket socket=new Socket("localhost",5000);
+       System.out.println("Connected!"); 
+      
+       BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+       BufferedReader keyboard=new BufferedReader(new InputStreamReader(System.in));
+PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
+String msg1,msg2;
+  while (true) {
+            System.out.print("Type text: ");
+msg1 = keyboard.readLine();
+            
+ 
+            // stop with "exit"
+            if (msg1.equalsIgnoreCase("exit")) {
+                System.out.println("Closing connection...");
+                break;
+            }
+              out.println(msg1);
+            msg2 = in.readLine();
+            System.out.println("From Server: "+msg2);
+          
+
+            
+    }
+      socket.close();  // <-- now it runs
+        System.out.println("Client closed.");
+}
+}
---
+++ ChatServer.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ChatServer{
+    public static void main(String[]args)throws Exception{
+ServerSocket server=new ServerSocket(5000);
+System.out.println("Waiting...");
+Socket socket=server.accept();
+System.out.println("Client connected.");
+
+ BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+       BufferedReader keyboard=new BufferedReader(new InputStreamReader(System.in));
+PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
+
+String msg1,msg2;
+ while ((msg1 = in.readLine()) != null) {
+
+            if (msg1.equalsIgnoreCase("exit")) {
+                System.out.println("Client closed connection.");
+                break;
+            }
+            System.out.println("From Client: " + msg1);
+            System.out.print("Type text: ");
+msg2=keyboard.readLine();
+
+            out.println(msg2);
+            
+        }
+        server.close();
+        socket.close();
+        System.out.println("Server closed.");
+    }
+}
